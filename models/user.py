38cdabf0b1cc74956756d01be4dd614f77e1cc31
Sholawasiu <sholawasiu92@gmail.com>
2024-05-19 10:32:35 -0700

    models

--- models/user.py
+++ models/user.py
@@ -7,7 +7,8 @@ class User(BaseModel):
     """Represent a User.
 
     Attributes:
-        email (str): The email of the user.
+        email (str):
+        The email of the user.
         password (str): The password of the user.
         first_name (str): The first name of the user.
         last_name (str): The last name of the user.
